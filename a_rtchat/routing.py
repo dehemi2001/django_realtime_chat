8bd8f94631759c7d8b44c73cb10cc4c762adc0e9
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-08 17:09:00 +0530

    Websocket connection

---
+++ a_rtchat/routing.py
@@ -0,0 +1,7 @@
+from channels.generic import websocket
+from django.urls import path
+from .consumers import *
+
+websocket_urlpatterns = [
+    path('ws/chatroom/<chatroom_name>', ChatroomConsumer.as_asgi()),
+]
