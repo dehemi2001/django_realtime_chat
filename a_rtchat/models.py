846c0565038ae2ada43c6197fe4ab5031c3d31e4
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-14 23:30:51 +0530

    Private Chat

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.conf import settings
+import shortuuid
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True)
+    group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
     users_online = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='online_in_groups', blank=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='chat_groups', blank=True)
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.group_name
