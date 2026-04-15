23506e10adfe1647ce4e7064437fdf3db1f5957b
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2026-04-15 21:40:27 +0530

    Group admin

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.admin import widgets
 from a_rtchat.models import GroupMessage
 from django.forms import ModelForm
 from django import forms
@@ -22,4 +23,15 @@ class NewGroupForm(ModelForm):
                 'maxlength': '300',
                 'autofocus': True
             })
+        }
+
+class ChatRoomEditForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name': forms.TextInput(attrs={
+                'class': 'p-4 txt-xl font-bold mb-4',
+                'maxlength': '300',
+            }),
         }
--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from .models import *
 from .forms import *
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -83,3 +84,46 @@ def create_groupchat(request):
         'form' : form
     }
     return render(request, 'a_rtchat/create_groupchat.html', context)
+
+@login_required
+def chatroom_edit_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+
+    form = ChatRoomEditForm(instance=chat_group)
+
+    if request.method == 'POST':
+        form = ChatRoomEditForm(request.POST, instance=chat_group)
+        if form.is_valid():
+            form.save()
+
+            remove_members = request.POST.getlist('remove_members')
+            for member_id in remove_members:
+                member = User.objects.get(id=member_id)
+                chat_group.members.remove(member)
+
+            return redirect('chatroom', chatroom_name)
+
+    context = {
+        'form': form,
+        'chat_group': chat_group
+    }
+
+    return render(request, 'a_rtchat/chatroom_edit.html', context)
+
+@login_required
+def chatroom_delete_view(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if request.user != chat_group.admin:
+        raise Http404()
+
+    if request.method == 'POST':
+        chat_group.delete()
+        messages.success(request, 'Chatroom deleted')
+        return redirect('home')
+
+    context = {
+        'chat_group': chat_group
+    }
+    return render(request, 'a_rtchat/chatroom_delete.html', context)
